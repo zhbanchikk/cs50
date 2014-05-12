4e07d2e2bfc14274f58353f9bc606cc36d8bd630
zhbanchikk <vitaly.kovrigin@gmail.com>
2014-05-11 20:12:28 -0400

    pset1 initial push

---
+++ dev/c/pset1/mario.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int
+main(void) {
+	int n;
+	do {
+		printf("Enter Height: ");	
+		n = GetInt();
+	}
+	while ((n < 0) || (n > 23));
+	string space = " ";
+	string hash = "#";
+
+	for (int i=1; i<=n; i++) {
+		int j = 1;
+		while (j<=n-i) {
+			printf("%s", space);
+			j++;
+		}
+		j = 1;
+		while (j<=i+1) {
+			printf("%s", hash);
+			j++;
+		}
+		printf("\n");
+	}
+}
+
+/*
+5
+    ## 4s 2h
+   ### 3s 3h
+  #### 2s 4h
+ ##### 1s 5h
+###### 0s 6h
+*/
