0988d56fd018eac0bcbcc7109777e8c53e14d83e
zhbanchikk <vitaly.kovrigin@gmail.com>
2014-05-13 00:18:59 -0400

    greedy

--- dev/c/pset1/greedy.c
+++ dev/c/pset1/greedy.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int
 main(void) {
+	int coins = 0;
+	float n;
+	do {
+		printf("Enter amount in dollars (e.g. 2.89): ");	
+		n = GetFloat();
+	}
+	while (n<=0.0);
+	int usd;
+	usd = round(n*100);
+	
+	while (usd > 0) {
+		if (usd >= 25) {
+			coins++;
+			usd = usd - 25;
+		}
+		else if (usd >= 10) {
+			coins++;
+			usd = usd - 10;
+		} 
+		else if (usd >= 5) {
+			coins++;
+			usd = usd - 5;
+		}
+		else if ( usd >= 1) {
+			coins++;
+			usd = usd - 1;
+		}
+	}
+	printf("number of coins is %d\n", coins);
 }
